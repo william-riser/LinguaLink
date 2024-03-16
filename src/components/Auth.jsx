1c8ca39c43794229097e405b01fff4a4fc609270
rohanparikh5 <rparikh931@student.fuhsd.org>
2024-03-16 13:14:14 -0700

    chat not implemented but coding finished

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -1,16 +1,27 @@
 import {auth, provider} from "";
-import {signInWithPopup} from 'firebase/auth'
+import {signInWithPopup} from 'firebase/auth';
+import "../styles/Auth.css";
+import Cookies from "universal-cookie";
 
-export const Auth = () => {
+const cookies = new Cookies();
 
+
+export const Auth = ({setIsAuth}) => {
     const signInWithGoogle = async () =>{
-        await signInWithPopup(auth, provider);
-    }
+        try {
+            const result = await signInWithPopup(auth, provider);
+            cookies.set("auth-token", result.user.refreshToken);
+            setIsAuth(true);
+          } catch (err) {
+            console.error(err);
+          }
+        };
 
-    return <div className = "auth">
+    return (<div className = "auth">
         <p>Sign In With Google to Continue</p>
         <button onClick={signInWithGoogle}>Sign In With Google</button>
         </div>
+    );
 }
 
 export default Auth;
